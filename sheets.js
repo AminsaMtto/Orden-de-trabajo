7c68bad104fd5e25d1a597c22e3e2092ffccb2db
AminsaMtto <aminsamtto@gmail.com>
2026-05-23 15:42:39 -0600

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -13,7 +13,7 @@ async function guardarOrden(datos) {
     params.set('hora_reporte',       datos.hora_reporte || '');
     params.set('quien_reporta',      datos.quien_reporta || '');
     params.set('centro_de_negocio',     datos.centro_de_negocio || '');
-    params.set('tipo_mantenimiento', datos.tipo_mantenimiento || ''); // CORREGIDO: tipo_mant -> tipo_mantenimiento
+    params.set('tipo_trabajo', datos.tipo_trabajo || '');
     params.set('equipo_reportado',   datos.equipo_reportado || ''); // CORREGIDO: equipo -> equipo_reportado
     params.set('no_control',         datos.no_control || '');
     params.set('falla_reportada',    datos.falla_reportada || ''); // CORREGIDO: falla -> falla_reportada, SIN TRUNCAMIENTO
